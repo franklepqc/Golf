052432a2cd615f7eb588df0cceaf94506939d2f4
François Lépine <francois.lepine@gmail.com>
2019-04-30 10:12:05 -0400

    Ajout du script de test.

---
+++ Golf.Biz.Tests/ExecuterTests.ps1
@@ -0,0 +1,8 @@
+﻿# Script pour l'exécution des tests.
+
+# Variables de travail.
+$cheminDesktop = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+$cheminRepertoire = $cheminDesktop + "\Examen\Question3\ResultatsTests\"
+
+# Exécution des tests
+dotnet test -r $cheminRepertoire -l trx --filter "TestCategory=Service de calcul du score final"
