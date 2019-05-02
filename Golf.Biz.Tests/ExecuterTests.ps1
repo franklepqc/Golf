09d48be42ebf1d1e8b3dfc7d2950a333b4da39f5
François Lépine <francois.lepine@gmail.com>
2019-05-02 15:13:42 -0400

    Exécution des tests, mais pas de résultats.

--- Golf.Biz.Tests/ExecuterTests.ps1
+++ Golf.Biz.Tests/ExecuterTests.ps1
@@ -1,8 +1,3 @@
 ﻿# Script pour l'exécution des tests.
-
-# Variables de travail.
-$cheminDesktop = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
-$cheminRepertoire = $cheminDesktop + "\Examen\Question3\ResultatsTests\"
-
 # Exécution des tests
-dotnet test -r $cheminRepertoire -l trx --filter "TestCategory=Service de calcul du score final"
+dotnet test --filter "TestCategory=Service de calcul du score final"
